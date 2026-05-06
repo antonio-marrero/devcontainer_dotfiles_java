c10977680cd5bf4113d6227a4bec60fee600f49f
Antonio Marrero <antonio.marrero.m@gmail.com>
2026-05-06 16:32:05 +0100

    Update setup.sh with needed software

--- setup.sh
+++ setup.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-# No need to copy cloned config from ~/dotfiles to ~ as we will link ~/dotfiles/nvim to the workspace nvim config folder
-# cp -rf .config/nvim ~/.config/.
+# Tree-sitter client is needed by nvim-treesitter
+if [ ! -f /usr/local/bin/tree-sitter ]
+then
+    wget https://github.com/tree-sitter/tree-sitter/releases/download/v0.26.8/tree-sitter-cli-linux-x64.zip
+    unzip tree-sitter-cli-linux-x64.zip
+    sudo mv tree-sitter /usr/local/bin/tree-sitter
+    sudo chmod +x /usr/local/bin/tree-sitter
+    rm tree-sitter-cli-linux-x64.zip
+fi
+
+# Set the locale to en_US.utf8 that is the one the docker image has installed.
+# If not bash tries to use the locale of the host (en_GB.utf8 in my case)
+echo '\n\nexport LANG=en_US.utf8' >> $HOME/.profile
+echo -e '\nexport LC_ALL=en_US.utf8' >> $HOME/.profile
+
+# ripgrep is used by neovim telescope
+sudo sudo apt-get update && apt-get install ripgrep
